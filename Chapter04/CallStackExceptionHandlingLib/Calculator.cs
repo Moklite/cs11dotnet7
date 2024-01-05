5e0a5e9b6ecf08a60ed930aa98072ad89a926591
pajayi <pajayi@axxess.com>
2024-01-05 13:45:32 +0100

    latest

---
+++ Chapter04/CallStackExceptionHandlingLib/Calculator.cs
@@ -0,0 +1,17 @@
+﻿namespace CallStackExceptionHandlingLib
+{
+	public class Calculator
+	{
+		public static void Gamma() // public so it can be called from outside
+		{
+			WriteLine("In Gamma");
+			Delta();
+		}
+		private static void Delta()
+		// private so it can only be called internally
+		{
+			WriteLine("In Delta");
+			File.OpenText("bad file path");
+		}
+	}
+} 
---
+++ Chapter05/Chapter05/PeopleApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using Packt.Shared;
+using Env = System.Environment;
+
+
+WriteLine(Env.OSVersion);
+WriteLine(Env.MachineName);
+WriteLine(Env.CurrentDirectory);
+
+Person bob = new();
+WriteLine(bob.ToString());
+bob.Name = "Bob Smith";
+bob.DateOfBirth = new DateTime(1965, 12, 22);
+bob.FavoriteAncientWonder =
+ WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
+WriteLine(
+ format: "{0}'s favorite wonder is {1}. Its integer is {2}.",
+ arg0: bob.Name,
+ arg1: bob.FavoriteAncientWonder,
+ arg2: (int)bob.FavoriteAncientWonder);
+
+bob.BucketList =
+ WondersOfTheAncientWorld.HangingGardensOfBabylon
+ | WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
+// bob.BucketList = (WondersOfTheAncientWorld)18;
+WriteLine($"{bob.Name}'s bucket list is {bob.BucketList}");
+
+bob.Children.Add(new Person { Name = "Alfred" });
+bob.Children.Add(new () { Name = "Zoe" });
+
+WriteLine($"{bob.Name} has {bob.Children.Count} children");
+foreach (var item in bob.Children)
+{
+	WriteLine($"{item.Name}");
+}
+
+for (int childIndex = 0; childIndex < bob.Children.Count; childIndex++)
+{
+	WriteLine($"> {bob.Children[childIndex].Name}");
+}
+
+Person alice = new()
+{
+	Name = "Alice Jones",
+	DateOfBirth = new(1998, 3, 7) // C# 9.0 or later
+};
+WriteLine(format: "{0} was born on {1:dd MMM yy}",
+ arg0: alice.Name,
+ arg1: alice.DateOfBirth);
