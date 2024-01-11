0ef2d0789740f2fdfd7d27f15be46dbdc7361e43
pajayi <pajayi@axxess.com>
2024-01-11 15:46:06 +0100

    null check tutorial

--- Chapter06/Chapter06/PeopleApp/Program.cs
+++ Chapter06/Chapter06/PeopleApp/Program.cs
@@ -7,32 +7,60 @@ Person harry = new()
 };
 harry.WriteToConsole();
 
-// non-generic lookup collection
-System.Collections.Hashtable lookupObject = new();
-lookupObject.Add(key: 1, value: "Alpha");
-lookupObject.Add(key: 2, value: "Beta");
-lookupObject.Add(key: 3, value: "Gamma");
-lookupObject.Add(key: harry, value: "Delta");
+//// non-generic lookup collection
+//System.Collections.Hashtable lookupObject = new();
+//lookupObject.Add(key: 1, value: "Alpha");
+//lookupObject.Add(key: 2, value: "Beta");
+//lookupObject.Add(key: 3, value: "Gamma");
+//lookupObject.Add(key: harry, value: "Delta");
 
-int key = 2; // look up the value that has 2 as its key
+//int key = 2; // look up the value that has 2 as its key
 
-WriteLine(format: "Key {0} has value: {1}",
- arg0: key,
- arg1: lookupObject[key]);
+//WriteLine(format: "Key {0} has value: {1}",
+// arg0: key,
+// arg1: lookupObject[key]);
 
-// look up the value that has harry as its key
-WriteLine(format: "Key {0} has value: {1}",
- arg0: harry,
- arg1: lookupObject[harry]);
+//// look up the value that has harry as its key
+//WriteLine(format: "Key {0} has value: {1}",
+// arg0: harry,
+// arg1: lookupObject[harry]);
 
-// generic lookup collection
-Dictionary<int, string> lookupIntString = new();
-lookupIntString.Add(key: 1, value: "Alpha");
-lookupIntString.Add(key: 2, value: "Beta");
-lookupIntString.Add(key: 3, value: "Gamma");
-lookupIntString.Add(key: 4, value: "Delta");
+//// generic lookup collection
+//Dictionary<int, string> lookupIntString = new();
+//lookupIntString.Add(key: 1, value: "Alpha");
+//lookupIntString.Add(key: 2, value: "Beta");
+//lookupIntString.Add(key: 3, value: "Gamma");
+//lookupIntString.Add(key: 4, value: "Delta");
 
-key = 3;
-WriteLine(format: "Key {0} has value: {1}",
- arg0: key,
- arg1: lookupIntString[key]);
+//key = 3;
+//WriteLine(format: "Key {0} has value: {1}",
+// arg0: key,
+// arg1: lookupIntString[key]);
+
+// assign a method to the Shout delegate
+harry.Shout += Harry_Shout;
+harry.Shout += Harry_Shout2;
+// call the Poke method that raises the Shout event
+harry.Poke();
+harry.Poke();
+harry.Poke();
+harry.Poke();
+
+Person?[] people =
+{
+ null,
+ new() { Name = "Simon" },
+ new() { Name = "Jenny" },
+ new() { Name = "Adam" },
+ new() { Name = null },
+  new() { Name = "Richard" }
+};
+OutputPeopleNames(people, "Initial list of people:");
+Array.Sort(people);
+OutputPeopleNames(people,
+ "After sorting using Person's IComparable implementation:");
+
+DisplacementVector dv1 = new(3, 5);
+DisplacementVector dv2 = new(-2, 7);
+DisplacementVector dv3 = dv1 + dv2;
+WriteLine($"({dv1.X}, {dv1.Y}) + ({dv2.X}, {dv2.Y}) = ({dv3.X},{ dv3.Y})");
