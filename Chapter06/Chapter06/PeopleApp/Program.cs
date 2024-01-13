4a1b4420d61f26fd4e274a168e589f27a282f849
pajayi <pajayi@axxess.com>
2024-01-13 15:09:23 +0100

    chp06 ending

--- Chapter06/Chapter06/PeopleApp/Program.cs
+++ Chapter06/Chapter06/PeopleApp/Program.cs
@@ -93,3 +93,12 @@ catch (PersonException ex)
 {
 	WriteLine(ex.Message);
 }
+
+string email1 = "pamela@test.com";
+string email2 = "ian&test.com";
+WriteLine("{0} is a valid e-mail address: {1}",
+ arg0: email1,
+ arg1: email1.IsValidEmail());
+WriteLine("{0} is a valid e-mail address: {1}",
+ arg0: email2,
+ arg1: StringExtensions.IsValidEmail(email2));
