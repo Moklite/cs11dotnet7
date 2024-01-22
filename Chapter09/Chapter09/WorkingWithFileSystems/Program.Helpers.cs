064da619924f3b5d71985ec16d170fd6311040e6
pajayi <pajayi@axxess.com>
2024-01-22 13:33:36 +0100

    json xml

---
+++ Chapter09/Chapter09/WorkingWithFileSystems/Program.Helpers.cs
@@ -0,0 +1,15 @@
+﻿namespace WorkingWithFileSystems
+{
+	partial class Program
+	{
+		static void SectionTitle(string title)
+		{
+			ConsoleColor previousColor = ForegroundColor;
+			ForegroundColor = ConsoleColor.Yellow;
+			WriteLine("*");
+			WriteLine($"* {title}");
+			WriteLine("*");
+			ForegroundColor = previousColor;
+		}
+	}
+}
