6a6e00fd6006d119e5fff111561b087e086f9d1d
pajayi <pajayi@axxess.com>
2024-01-23 00:18:25 +0100

    query

--- Chapter10/Chapter10/WorkingWithEFCore/Program.cs
+++ Chapter10/Chapter10/WorkingWithEFCore/Program.cs
@@ -1,15 +1,70 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Packt.Shared;
 
-QueryingCategories();
+//QueryingCategories();
+//FilteredIncludes();
+QueryingProducts();
 
 
+static void QueryingProducts()
+{
+	using (Northwind db = new())
+	{
+		SectionTitle("Products that cost more than a price, highest at top.");
+
+		string? input;
+		decimal price;
+
+		do
+		{
+			Write("Enter a product price: ");
+			input = ReadLine();
+		} while (!decimal.TryParse(input, out price));
+
+		IQueryable<Product>? products = db.Products?.Where(p => p.Cost > price).OrderByDescending(p => p.Cost);
+
+		if ((products is null) || (!products.Any()))
+		{
+			Fail("No products found.");
+			return;
+		}
+		foreach (Product p in products)
+		{
+			WriteLine(
+			"{0}: {1} costs {2:$#,##0.00} and has {3} in stock.",
+			p.ProductId, p.ProductName, p.Cost, p.Stock);
+		}
+	}
+}
 
 static void FilteredIncludes()
 {
 	using(Northwind db = new())
 	{
+		SectionTitle("Products with a minimum number of units in stock.");
 
+		string? input;
+		int stock;
+		do
+		{
+			WriteLine("Enter minimum units in stock: ");
+			input = ReadLine();
+		} while (!int.TryParse(input, out stock));
+
+		IQueryable<Category>? categories = db.Categories?.Include(c => c.Products.Where(p => p.Stock >= stock));
+		if((categories is null) || (!categories.Any()))
+		{
+			Fail("No categories found");
+			return;
+		}
+		foreach (Category c in categories)
+		{
+			WriteLine($"{c.CategoryName} has {c.Products.Count} products with a minimum of {stock} units in stock.");
+			 foreach (Product p in c.Products)
+			 {
+				WriteLine($" {p.ProductName} has {p.Stock} units in stock.");
+			 }
+		}
 	}
 }
 
